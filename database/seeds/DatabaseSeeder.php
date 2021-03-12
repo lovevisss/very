16e0e0b7325ecc5927d8b4eef84533791d1a16a3
zsy <530822987@qq.com>
2021-03-12 13:57:08 +0800

    add device link to rooms fix2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ArticlesTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
         $this->call(RoomSeeder::class);
+        $this->call(DeviceSeeder::class);
     }
 }
