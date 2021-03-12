f678393d251732fe295cde124c63ad51694bae9c
zsy <530822987@qq.com>
2021-03-12 14:13:24 +0800

    add device link to rooms test

--- resources/views/room/room.blade.php
+++ resources/views/room/room.blade.php
@@ -3,11 +3,9 @@
 @section('content')
     <h>search result</h>
 
-    <p>地址</p>{{$room->building}} - {{$room->room}}
+    <p>地址</p>{{$room->building}} - {{$room->name}}
 
 
     devices
-        @foreach($room->devices() as $device)
-            {{$device->type}}-{{$device->vlan}}
-        @endforeach
+{{ dd($room->devices)}}
 @endsection
