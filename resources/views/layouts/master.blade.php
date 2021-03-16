b27989f0e9f82f102101f842151d55fc8e7b0ee8
zsy <530822987@qq.com>
2021-03-16 10:14:54 +0800

    add nav fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
 		<title>@yield('title')</title>
 		{{Html::style('css/main.css')}}
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css">
 		@yield('style')
 	</head>
 
