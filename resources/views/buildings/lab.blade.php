144b1b756cce1b80cb5d02af0af5f1482a56ff50
ns <530822987@qq.com>
2021-03-19 10:46:12 +0800

    paginate fix

--- resources/views/buildings/lab.blade.php
+++ resources/views/buildings/lab.blade.php
@@ -24,7 +24,7 @@
         @endforeach
     </ul>
     <div class="pagination">
-        {{ $room->links() }}
+        {{ $rooms->links() }}
     </div>
     <div class="clearfix"></div>
     <form action="{{route('addRoom')}}" method="post">
