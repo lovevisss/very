9c6a0002c5f635c0d9bd53ab2d55f57af84bb6ed
ns <530822987@qq.com>
2021-04-15 12:37:53 +0800

    test

--- resources/views/Devices/index.blade.php
+++ resources/views/Devices/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <article>
         <h3>{{$room->name}}</h3>
-{{--        <span class="">{{$room->owner()}}</span>--}}
+        <span class="">{{$room->owner()}}</span>
 {{--        <ul>--}}
 {{--        @foreach($room->devices as $device)--}}
 {{--            <li>--}}
