a8b60b4b4c5d1c3300cb9d3602269da185c4c35c
ns <530822987@qq.com>
2021-04-15 12:39:04 +0800

    test

--- resources/views/Devices/index.blade.php
+++ resources/views/Devices/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <article>
         <h3>{{$room->name}}</h3>
-        <span class="">{{$room->owner()}}</span>
-{{--        <ul>--}}
-{{--        @foreach($room->devices as $device)--}}
-{{--            <li>--}}
-{{--                <a href="{{route('device.single', $device->id)}}">{{$device->type}}-{{$device->vlan}}</a>--}}
-{{--            </li>--}}
-{{--        @endforeach--}}
-{{--        </ul>--}}
+        <span class="">{{$room->owner()->name}}</span>
+        <ul>
+        @foreach($room->devices as $device)
+            <li>
+                <a href="{{route('device.single', $device->id)}}">{{$device->type}}-{{$device->vlan}}</a>
+            </li>
+        @endforeach
+        </ul>
     </article>
 
     <section class="pagination">
